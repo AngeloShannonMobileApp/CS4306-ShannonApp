7225f2df5502ae3e34aade3935057b013cc52095
CadeCampbell <campbellcade217@gmail.com>
2022-10-16 22:11:12 -0500

    maps testing

--- app/src/main/java/edu/angelo/shannonmobileapp/MainActivity.java
+++ app/src/main/java/edu/angelo/shannonmobileapp/MainActivity.java
@@ -3,12 +3,27 @@ package edu.angelo.shannonmobileapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMapsActivity();
+            }
+        });
+    }
+
+    public void openMapsActivity(){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
