0df23db1a03fcdea43a0df6f443e9f8031177af0
CadeCampbell <campbellcade217@gmail.com>
2022-10-18 19:24:31 -0500

    Google Maps API prototype notes

--- app/src/main/java/edu/angelo/shannonmobileapp/MapsActivity.java
+++ app/src/main/java/edu/angelo/shannonmobileapp/MapsActivity.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.MapStyleOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
