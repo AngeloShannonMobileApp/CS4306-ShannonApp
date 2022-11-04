dda1d2795b05ed0bb73a8cce8f86a6dbe213d4ba
Jmoore63 <jmoore63@angelo.edu>
2022-11-04 15:16:36 -0500

    Updated to add homepage

---
+++ app/src/main/java/edu/angelo/shannonmobileapp/HomePageActivity.java
@@ -0,0 +1,15 @@
+package edu.angelo.shannonmobileapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+
+}
--- app/src/main/java/edu/angelo/shannonmobileapp/MainActivity.java
+++ app/src/main/java/edu/angelo/shannonmobileapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private Button buttonHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +21,23 @@ public class MainActivity extends AppCompatActivity {
                 openMapsActivity();
             }
         });
+
+        buttonHome = findViewById(R.id.loginButton);
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View vHome) {
+                openHomePageActivity();
+            }
+        });
     }
 
     public void openMapsActivity(){
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
+    }
+
+    public void openHomePageActivity() {
+        Intent intentHome = new Intent(this, HomePageActivity.class);
+        startActivity(intentHome);
     }
 }
